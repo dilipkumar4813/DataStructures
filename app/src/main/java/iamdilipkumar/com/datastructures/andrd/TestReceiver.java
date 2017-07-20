db87e0a0e8d44b80f74f88dc37e45ace6020b744
Dilip <dilipkumar4813@gmail.com>
2017-07-20 22:54:13 +0530

    Templates for provider, receiver and service

---
+++ app/src/main/java/iamdilipkumar/com/datastructures/andrd/TestReceiver.java
@@ -0,0 +1,20 @@
+package iamdilipkumar.com.datastructures.andrd;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created on 20/07/17.
+ *
+ * @author dilipkumar4813
+ * @version 1.0
+ */
+
+public class TestReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/iamdilipkumar/com/datastructures/andrd/TestService.java
@@ -0,0 +1,38 @@
+package iamdilipkumar.com.datastructures.andrd;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+
+/**
+ * Created on 20/07/17.
+ *
+ * @author dilipkumar4813
+ * @version 1.0
+ */
+
+public class TestService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
