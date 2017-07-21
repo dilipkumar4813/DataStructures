ecd8125e1df2de5fa7124a4aebf98b0fbde4f495
Dilip <dilipkumar4813@gmail.com>
2017-07-21 22:30:54 +0530

    Default methods for SQLiteOpenHelper

--- app/src/main/java/iamdilipkumar/com/datastructures/andrd/TestSqlDatabase.java
+++ app/src/main/java/iamdilipkumar/com/datastructures/andrd/TestSqlDatabase.java
@@ -1,8 +1,32 @@
 package iamdilipkumar.com.datastructures.andrd;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
- * Created by dilipkumar4813 on 21/07/17.
+ * Created on 21/07/17.
+ *
+ * @author dilipkumar4813
+ * @version 1.0
  */
 
-public class TestSqlDatabase {
+public class TestSqlDatabase extends SQLiteOpenHelper{
+
+    private final static String DATABASE_NAME = "testdatabase";
+    private final static int DATABASE_VERSION = 1;
+
+    public TestSqlDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
